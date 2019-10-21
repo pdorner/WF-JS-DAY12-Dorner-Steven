0ea0acf48e9428cce8f63807a34b4276ad06974a
Philipp Dorner <pdorner@gmx.net>
2019-10-21 14:35:51 +0200

    fast fertig

--- Grocery/js/script.js
+++ Grocery/js/script.js
@@ -1,12 +1,11 @@
 /*jshint esversion: 6 */
 
-let productlist = JSON.parse(products);
-
 $(document).ready(function () {
     $("main").append("<div class='flex'></div>");
-    $("flex").css({"display": "flex", "justify-content": "space-between", "flex-wrap:":"wrap"});
-    for (i=0;i<productlist.length; i++){
-        $("flex").append(`<h1>${productlist[i].name}</h1><img src=${productlist[i].img}><h4>${productlist[i].price}</h4><p>${productlist[i].stock}</p>`);
+    $(".flex").css({"display": "flex", "justify-content": "space-between", "flex-wrap":"wrap"});
+    for (i=0;i<products.length; i++){
+        $(".flex").append(`<div class = "item"><h1>${products[i].name}</h1><img src=${products[i].img}><h4>${products[i].price} €</h4><p>${products[i].stock} in Stock</p></div>`);
+
     }
     
 });
